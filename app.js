9cae8204f53cfc68668adbaebb9b162eac653105
Daniel Bednarek <Daniel_Bednarek@epam.com>
2023-09-01 18:38:12 +0200

    exercise 2 done

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 const os = require("os");
+const childProcess = require("child_process");
+const fs = require("fs");
 
-// check operating system
-console.log(os.type());
+const execProcess = (command) => {
+    let roundCounter = 0;
+	setInterval(() => {
+		childProcess.exec(command, (error, stdout, stderr) => {
+			childProcess.execSync('powershell "Clear"');
+            process.stdout.write(`Process_Name - CPU Usage - RAM Usage\n`)
+			process.stdout.write(`${stdout}`);
+			stderr ? console.log(`stderr: ${stderr}`) : null;
 
+			if (error !== null) {
+				console.log(`error: ${error}`);
+			}
+
+            //assuming interval will stay 1000ms
+            if (roundCounter === 0 || roundCounter % 60 === 0) {
+                fs.appendFile('activityMonitor.log', `${Math.floor(Date.now() / 1000)}:\n Process_Name - CPU Usage - RAM Usage\n${stdout}\n`, (err)=>{
+                    if (err) throw err;
+                })
+            }
+            console.log(roundCounter)
+            roundCounter++;
+		});
+	}, 1000); // cannot do 100ms - PC freezes
+};
+
+// check operating system (tested only on Windows 10)
 if (os.platform() === "win32") {
-	console.log("windows code block");
+	execProcess(
+		`powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + ' - ' + $_.CPU + ' - ' + $_.WorkingSet + ' bytes'}"`
+	);
 } else if (os.platform() === "darwin" || os.platform() === "linux") {
-	console.log("MacOS/Linux code block");
+	execProcess(`ps -A -o %cpu,%mem,comm | sort -nr | head -n 1`);
+} else {
+	console.log("OS not supported!");
 }
