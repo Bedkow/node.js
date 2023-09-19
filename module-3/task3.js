208c2dec244bba12575eb05158687751dd342c64
Daniel Bednarek <Daniel_Bednarek@epam.com>
2023-09-19 12:27:47 +0200

    resolve header problems

--- module-3/task3.js
+++ module-3/task3.js
@@ -7,17 +7,27 @@ const CSVstream = readline.createInterface({
     input: fs.createReadStream('./csvdirectory/nodejs-m3-t3.csv'),
 });
 
+let lineNumber = 1;
 //line read event callback
-CSVstream.on('line', (line) => {
+
+    CSVstream.on('line', (line) => {
     csvtojson({
         noheader: true,
-        output: "json"
+        headers: ["Book", "Author", "Amount", "Price"],
+        output: "json",
+        downstreamFormat: "line"
     })
     .fromString(line)
     .then((row)=>{
         // console.log(row)
-        fs.appendFile('./txtdirectory/nodejs-m3.t3.txt', JSON.stringify(row), (err)=>{
-            if (err) throw err;
-        })
+        if (lineNumber > 1) {
+            fs.appendFile('./txtdirectory/nodejs-m3.t3.txt', JSON.stringify(row), (err)=>{
+                if (err) throw err;
+            })
+        } else {
+            null;
+        }
+        lineNumber++;
     })
 });
+
