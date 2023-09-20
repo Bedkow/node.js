dd3fdf91ae157c92997a9964c35f659ce97ff47e
Daniel Bednarek <Daniel_Bednarek@epam.com>
2023-09-20 17:49:24 +0200

    modify jest config

--- module-4/jest.config.js
+++ module-4/jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.ts?$': 'ts-jest',
-    },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  };
+	preset: "ts-jest",
+	testEnvironment: "node",
+	roots: ["./src"],
+	passWithNoTests: true,
+	moduleNameMapper: {
+		"^axios$": "axios/dist/node/axios.cjs",
+	},
+	testMatch: ["**/?(*.)test.ts"],
+	collectCoverageFrom: ["src/**/*.{ts}"],
+	coverageReporters: ["text-summary"],
+};
