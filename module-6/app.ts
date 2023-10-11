def87c21005c0a0504d60facf696bd72085f50d6
Daniel Bednarek <Daniel_Bednarek@epam.com>
2023-10-11 12:13:42 +0200

    add infrastructure and swagger

--- module-6/app.ts
+++ module-6/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import swaggerUiDist from 'swagger-ui-dist';
+const pathToSwaggerUi = swaggerUiDist.absolutePath();
 
 const app = express();
 
+app.use(express.static(pathToSwaggerUi));
+
 app.listen(3500, () => {
-    console.log('express')
+    console.log('express listening')
 })
