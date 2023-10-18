bdcf795461a3367af4486da3cce429d143e4450a
Daniel Bednarek <Daniel_Bednarek@epam.com>
2023-10-18 15:20:57 +0200

    minor fixes

--- module-6/src/businessLogic.service.ts
+++ module-6/src/businessLogic.service.ts
@@ -1,25 +1,23 @@
 // Or Service layer. The middle layer. All business logic related to our application might be in this layer. This layer knows about our application entities such as user, order, comment, post, and so on. It doesn't care what protocol is used for transferring data to a client, or whether data will be sent in a body or in a header.
+import Joi from 'joi';
 import { ProductEntity } from "./entities/product.entity.ts";
 import { getAllCarts, getAllProducts } from "./dataAccess.repository.ts";
 import { CartEntity } from "./entities/cart.entity.ts";
-import Joi from 'joi';
 
 export const findProductByID = (id: string): ProductEntity | undefined => {
 	const allProducts = getAllProducts();
 
-	const foundProduct = allProducts.find((el: ProductEntity) => {
+	return allProducts.find((el: ProductEntity) => {
 		return el.id === id;
 	});
-	return foundProduct;
 };
 
 export const findCartByUserID = (id: string): CartEntity | undefined => {
 	const allCarts = getAllCarts();
 
-	const foundCart = allCarts.find((el: CartEntity) => {
+	return allCarts.find((el: CartEntity) => {
 		return el.userId === id
 	});
-	return foundCart;
 }
 
 export const validateCartSchema = (cartObject: CartEntity): any => {
